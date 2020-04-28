4980e9333588a68f0856278f32b01b34874be79b
parasdec2 <49130632+parasdec2@users.noreply.github.com>
2020-04-28 20:59:55 +0530

    Footer updated

--- client/src/core/Base.js
+++ client/src/core/Base.js
@@ -19,8 +19,10 @@ export default function Base({
       </div>
       <footer className="footer mt-auto py-3">
         <div className="container-fluid bg-success text-white text-center py-3">
-          <h4>If you got any questions feel free to reach out!</h4>
-          <button className="btn btn-warning btn-lg">Contact Us</button>
+          <h4>Made by Paras Garg</h4>
+          <a className="nav-link" href="https://www.linkedin.com/in/parasdec2/">
+            <button className="btn btn-info btn-lg">Linked In Profile</button>
+          </a>
         </div>
         <div className="container">
           <span className="text-muted">
